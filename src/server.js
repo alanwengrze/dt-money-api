98d8bfeddec2760f6ee1077e20038e8d55f520e1
alan <alanwengrzee@gmail.com>
2024-12-10 22:34:58 -0300

    :bug: fix: url changed

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://ignite-dt-money-nu.vercel.app/'
+  origin: 'https://ignite-dt-money-nu.vercel.app'
 }));
 
 app.use(routes);
